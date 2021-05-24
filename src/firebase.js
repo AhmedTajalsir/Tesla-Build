87b5c18b9f38ff7bc73d330a5213f8eef379621e
AhmedTajalsir <ah.alkhier2@gmail.com>
2021-05-25 04:07:00 +0530

    init

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+const firestore = firebaseApp.initietizeApp ({
+
+    apiKey: "AIzaSyCSraJSGmGskYkC3B7lIjgNGswTCeAu6fY",
+    authDomain: "netflix-build-baec8.firebaseapp.com",
+    projectId: "netflix-build-baec8",
+    storageBucket: "netflix-build-baec8.appspot.com",
+    messagingSenderId: "697434790468",
+    appId: "1:697434790468:web:186b7ec05fe629e37d3a11",
+    measurementId: "G-E1F1J783CC"
+})
+const db = firebaseApp.firestore;
+const auth = firebaseApp
